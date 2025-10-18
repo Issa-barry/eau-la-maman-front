27f43bc51e3b801cd911787739bd3a53227dc587
Issa-barry <issabarry67@gmail.com>
2025-10-18 13:31:25 +0200

    money format

--- src/app/demo/components/dashboards/banking/banking.dashboard.module.ts
+++ src/app/demo/components/dashboards/banking/banking.dashboard.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/pipes/shared.module';
 import { SkeletonModule } from 'primeng/skeleton';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { PipeModule } from 'src/app/demo/pipes/pipe.module';
  
 @NgModule({
     imports: [
@@ -31,7 +32,8 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
         SharedModule,
         SkeletonModule,
         TableModule,
-        ProgressSpinnerModule   
+        ProgressSpinnerModule,
+        PipeModule 
         
     ],
     declarations: [BankingDashboardComponent],

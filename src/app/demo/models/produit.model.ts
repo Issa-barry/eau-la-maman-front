418beb12ede63c7909ec9cdd3b84e477821ad809
Issa-barry <issabarry67@gmail.com>
2025-10-18 15:57:45 +0200

    produit ok

--- src/app/demo/models/produit.model.ts
+++ src/app/demo/models/produit.model.ts
@@ -1,32 +1,32 @@
 import { TypeProduitEnum } from "../enums/typeProduit.enum";
 
 export class Produit {
-  id?: number;
-  code?: string;
-  nom: string;
-  type: TypeProduitEnum = TypeProduitEnum.Vente
-  prix_vente: number;
-  quantite_stock: number;
-  prix_achat?: number;
-  cout?: number;
-  image?: string;
-  categorie: string;
- 
-  created_at?: string;
-  updated_at?: string;
-  statut?:string;
-   imagePreview?: string;
-    
+    id?: number;
+    code?: string;
+    nom: string;
+    type: TypeProduitEnum = TypeProduitEnum.Vente;
+    prix_usine: number;
+    prix_vente: number;
+    prix_achat?: number;
+    quantite_stock: number;
+    cout?: number;
+    image?: string;
+    categorie: string;
 
-  constructor()
-      {
-          this.nom = ""; 
-          this.categorie = "";
-          this.prix_vente = 0;
-          this.quantite_stock = 0;
-          this.image ="defaut1.png";
-          this.prix_achat = 0;
-          this.cout = 0;
-          
-      }
-  }  
+    created_at?: string;
+    updated_at?: string;
+    statut?: string;
+    imagePreview?: string;
+
+
+    constructor() {
+        this.nom = "";
+        this.categorie = "";
+        this.prix_usine = 0;
+        this.prix_vente = 0;
+        this.prix_achat = 0;
+        this.cout = 0;
+        this.quantite_stock = 0;
+        this.image = "defaut1.png";
+    }
+}  
